13b2defbb8d634ce1ac666f9d99b04ae025f1db4
root <nagaraja.hm.0001@gmail.com>
2022-02-03 12:18:33 +0100

    Update: submodule MemIf

--- LATEST/MemIf_EcuM.h
+++ LATEST/MemIf_EcuM.h
@@ -1,14 +1,15 @@
+#pragma once
 /*****************************************************/
-/* File   : MemIf.cpp                                */
+/* File   : MemIf_EcuM.h                             */
 /* Author : Naagraaj HM                              */
 /*****************************************************/
 
 /*****************************************************/
 /* #INCLUDES                                         */
 /*****************************************************/
-#include "MemIf.h"
+#include "Compiler_Cfg_MemIf.h"
 
-#include "MemIf_EcuM.h"
+#include "EcuM_Client.h"
 
 /*****************************************************/
 /* #DEFINES                                          */
@@ -21,6 +22,13 @@
 /*****************************************************/
 /* TYPEDEFS                                          */
 /*****************************************************/
+class class_MemIf_EcuM : public class_EcuM_Client{
+   public:
+/*****************************************************/
+/* FUNCTIONS                                         */
+/*****************************************************/
+      FUNC(void, MEMIF_CODE) InitFunction(void);
+};
 
 /*****************************************************/
 /* CONSTS                                            */
@@ -33,42 +41,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-class_MemIf MemIf;
-class_MemIf_EcuM MemIf_EcuM;
-class_EcuM_Client *EcuM_Client_ptr_MemIf = &MemIf_EcuM;
-
-/*****************************************************/
-/* FUNCTIONS                                         */
-/*****************************************************/
-FUNC(void, MEMIF_CODE) class_MemIf_EcuM::InitFunction(void){
-}
-
-FUNC(void, MEMIF_CODE) class_MemIf::SetMode(void){
-}
-
-FUNC(void, MEMIF_CODE) class_MemIf::Read(void){
-}
-
-FUNC(void, MEMIF_CODE) class_MemIf::Write(void){
-}
-
-FUNC(void, MEMIF_CODE) class_MemIf::Cancel(void){
-}
-
-FUNC(void, MEMIF_CODE) class_MemIf::GetStatus(void){
-}
-
-FUNC(void, MEMIF_CODE) class_MemIf::GetJobResult(void){
-}
-
-FUNC(void, MEMIF_CODE) class_MemIf::GetVersionInfo(void){
-}
-
-FUNC(void, MEMIF_CODE) class_MemIf::InvalidateBlock(void){
-}
-
-FUNC(void, MEMIF_CODE) class_MemIf::EraseImmediateBlock(void){
-}
+extern class_EcuM_Client *EcuM_Client_ptr_MemIf;
 
 /*****************************************************/
 /* EOF                                               */
