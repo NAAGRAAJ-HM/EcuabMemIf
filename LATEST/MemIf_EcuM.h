ef2f34cf292e57169f6f462b4f8f324f59680d4c
root <nagaraja.hm.0001@gmail.com>
2022-02-25 12:06:45 +0100

    Update: submodule MemIf

--- LATEST/MemIf_EcuM.h
+++ LATEST/MemIf_EcuM.h
@@ -27,8 +27,8 @@ class interface_MemIf_EcuM : public interface_EcuM_Client{
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-      FUNC(void, MEMIF_CODE) InitFunction   (void);
-      FUNC(void, MEMIF_CODE) DeInitFunction (void);
+      virtual FUNC(void, MEMIF_CODE) InitFunction   (void) = 0;
+      virtual FUNC(void, MEMIF_CODE) DeInitFunction (void) = 0;
 };
 
 /*****************************************************/
