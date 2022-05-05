c03ca84196a8a0212699d091e0423d2cb6630607
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-05 10:07:43 +0000

    Update: AUTOSAR basic software ECU abstraction module MemIf

--- LATEST/MemIf.cpp
+++ LATEST/MemIf.cpp
@@ -87,12 +87,10 @@ FUNC(void, MEMIF_CODE) module_MemIf::InitFunction(
    if(E_OK != IsInitDone){
 #endif
       if(NULL_PTR != lptrCfgModule){
-         if(STD_LOW){
-            // check lptrCfgModule for memory faults
+         if(STD_HIGH){
             lptrCfg = lptrCfgModule;
          }
          else{
-            // use PBcfgCanIf as back-up configuration
             lptrCfg = &PBcfgMemIf;
          }
       }
