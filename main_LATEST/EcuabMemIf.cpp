4a3a409d72a688ac33d869dc3241dd6baae307ac
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-04 19:55:03 +0200

    Update: AUTOSAR basic software ECU abstraction module for memory stack - interface layer

---
+++ main_LATEST/EcuabMemIf.cpp
@@ -0,0 +1,185 @@
+/******************************************************************************/
+/* File   : EcuabMemIf.cpp                                                         */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Module.hpp"
+#include "EcuabMemIf.hpp"
+#include "infEcuabMemIf_Imp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define MEMIF_AR_RELEASE_VERSION_MAJOR                                         4
+#define MEMIF_AR_RELEASE_VERSION_MINOR                                         3
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(MEMIF_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible MEMIF_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(MEMIF_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible MEMIF_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+VAR(module_EcuabMemIf, MEMIF_VAR) EcuabMemIf;
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, MEMIF_CODE) module_EcuabMemIf::InitFunction(
+      CONSTP2CONST(ConstModule_TypeAbstract, MEMIF_CONST,       MEMIF_APPL_CONST) lptrConstModule
+   ,  CONSTP2CONST(CfgModule_TypeAbstract,   MEMIF_CONFIG_DATA, MEMIF_APPL_CONST) lptrCfgModule
+){
+#if(STD_ON == EcuabMemIf_InitCheck)
+   if(
+         E_OK
+      != IsInitDone
+   ){
+#endif
+      if(
+            (NULL_PTR != lptrConstModule)
+         && (NULL_PTR != lptrCfgModule)
+      ){
+         lptrConst = (const ConstEcuabMemIf_Type*)lptrConstModule;
+         lptrCfg   = lptrCfgModule;
+      }
+      else{
+#if(STD_ON == EcuabMemIf_DevErrorDetect)
+         ServiceDet_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
+#if(STD_ON == EcuabMemIf_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == EcuabMemIf_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  MEMIF_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, MEMIF_CODE) module_EcuabMemIf::DeInitFunction(
+   void
+){
+#if(STD_ON == EcuabMemIf_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == EcuabMemIf_InitCheck)
+      IsInitDone = E_NOT_OK;
+   }
+   else{
+#if(STD_ON == EcuabMemIf_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  MEMIF_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, MEMIF_CODE) module_EcuabMemIf::MainFunction(
+   void
+){
+#if(STD_ON == EcuabMemIf_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == EcuabMemIf_InitCheck)
+   }
+   else{
+#if(STD_ON == EcuabMemIf_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  MEMIF_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, MEMIF_CODE) module_EcuabMemIf::McalCancel(
+   void
+){
+}
+
+FUNC(void, MEMIF_CODE) module_EcuabMemIf::Read(
+   void
+){
+}
+
+FUNC(void, MEMIF_CODE) module_EcuabMemIf::Write(
+   void
+){
+}
+
+FUNC(void, MEMIF_CODE) module_EcuabMemIf::SetMode(
+   void
+){
+}
+
+FUNC(void, MEMIF_CODE) module_EcuabMemIf::GetStatus(
+   void
+){
+}
+
+FUNC(void, MEMIF_CODE) module_EcuabMemIf::GetJobResult(
+   void
+){
+}
+
+FUNC(void, MEMIF_CODE) module_EcuabMemIf::InvalidateBlock(
+   void
+){
+}
+
+FUNC(void, MEMIF_CODE) module_EcuabMemIf::EraseImmediateBlock(
+   void
+){
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
---
+++ main_LATEST/EcuabMemIf_core.hpp
@@ -0,0 +1,66 @@
+#pragma once
+/******************************************************************************/
+/* File   : EcuabMemIf_core.hpp                                                    */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "CompilerCfg_EcuabMemIf.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define MEMIF_CORE_FUNCTIONALITIES                                             \
+              FUNC(void, MEMIF_CODE) SetMode             (void);               \
+              FUNC(void, MEMIF_CODE) Read                (void);               \
+              FUNC(void, MEMIF_CODE) Write               (void);               \
+              FUNC(void, MEMIF_CODE) McalCancel              (void);               \
+              FUNC(void, MEMIF_CODE) GetStatus           (void);               \
+              FUNC(void, MEMIF_CODE) GetJobResult        (void);               \
+              FUNC(void, MEMIF_CODE) InvalidateBlock     (void);               \
+              FUNC(void, MEMIF_CODE) EraseImmediateBlock (void);               \
+
+#define MEMIF_CORE_FUNCTIONALITIES_VIRTUAL                                     \
+      virtual FUNC(void, MEMIF_CODE) SetMode             (void) = 0;           \
+      virtual FUNC(void, MEMIF_CODE) Read                (void) = 0;           \
+      virtual FUNC(void, MEMIF_CODE) Write               (void) = 0;           \
+      virtual FUNC(void, MEMIF_CODE) McalCancel              (void) = 0;           \
+      virtual FUNC(void, MEMIF_CODE) GetStatus           (void) = 0;           \
+      virtual FUNC(void, MEMIF_CODE) GetJobResult        (void) = 0;           \
+      virtual FUNC(void, MEMIF_CODE) InvalidateBlock     (void) = 0;           \
+      virtual FUNC(void, MEMIF_CODE) EraseImmediateBlock (void) = 0;           \
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class class_EcuabMemIf_Functionality{
+   public:
+      MEMIF_CORE_FUNCTIONALITIES_VIRTUAL
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
