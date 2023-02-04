63a8d6e983f34790ef5a1f1ef9295cecdd095d88
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-04 19:30:36 +0100

    Update: AUTOSAR basic software ECU abstraction module for memory stack - interface layer

--- InfineonTLE9879EvalKit_R2023/infEcuabMemIf_ServiceNvM_Types.hpp
+++ InfineonTLE9879EvalKit_R2023/infEcuabMemIf_ServiceNvM_Types.hpp
@@ -10,9 +10,11 @@
 class NvM_BlocksRom_EcuabMemIf_Type:
       public ConstModule_TypeAbstract
 {
-/******************************************************************************/
-/* CONSTS                                                                     */
-/******************************************************************************/
+};
+
+class NvM_BlocksNv_EcuabMemIf_Type:
+      public CfgModule_TypeAbstract
+{
 };
 
 /******************************************************************************/
