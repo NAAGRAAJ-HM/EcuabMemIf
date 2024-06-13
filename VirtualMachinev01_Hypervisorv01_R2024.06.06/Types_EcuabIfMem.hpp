d03741ce8bcf06d8bb7f3131e7ae5f6b0a9a161f
Raajnaag HULIYAPURADA MATA <nagaraja.hm.0001@gmail.com>
2024-06-13 23:26:52 +0530

    Initial commit

---
+++ VirtualMachinev01_Hypervisorv01_R2024.06.06/Types_EcuabIfMem.hpp
@@ -0,0 +1,43 @@
+#ifndef MEMIF_TYPES_H
+#define MEMIF_TYPES_H
+
+#define MEMIF_TYPES_AR_RELEASE_MAJOR_VERSION                                (4u)
+#define MEMIF_TYPES_AR_RELEASE_MINOR_VERSION                                (0u)
+#define MEMIF_TYPES_AR_RELEASE_REVISION_VERSION                             (3u)
+#define MEMIF_TYPES_SW_MAJOR_VERSION                                        (3u)
+#define MEMIF_TYPES_SW_MINOR_VERSION                                        (3u)
+#define MEMIF_TYPES_SW_PATCH_VERSION                                        (1u)
+#define MEMIF_E_NO_ERROR                                                 (0x00u)
+#define MEMIF_E_PARAM_DEVICE                                             (0x01u)
+#define MEMIF_E_PARAM_POINTER                                            (0x02u)
+#define MEMIF_JOB_CANCELLED                                   MEMIF_JOB_CANCELED
+
+typedef enum{
+      MEMIF_UNINIT
+   ,  MEMIF_IDLE
+   ,  MEMIF_BUSY
+   ,  MEMIF_BUSY_INTERNAL
+}MemIf_StatusType;
+
+typedef enum{
+      MEMIF_JOB_OK
+   ,  MEMIF_JOB_FAILED
+   ,  MEMIF_JOB_PENDING
+   ,  MEMIF_JOB_CANCELED
+   ,  MEMIF_BLOCK_INCONSISTENT
+   ,  MEMIF_BLOCK_INVALID
+}MemIf_JobResultType;
+
+typedef enum{
+      MEMIF_MODE_SLOW
+   ,  MEMIF_MODE_FAST
+}MemIf_ModeType;
+
+typedef   P2VAR(uint8, AUTOMATIC, MEMIF_APPL_DATA) MemIf_DataPtr_pu8;
+typedef P2CONST(uint8, AUTOMATIC, MEMIF_APPL_DATA) MemIf_ConstDataPtr_pu8;
+
+typedef uint32 MemIf_AddressType;
+typedef uint32 MemIf_LengthType;
+
+#endif
+
